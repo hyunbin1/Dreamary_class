0944aad985f058bcc811e16e8cec707ef48537d2
hyunbin1 <kimgusqls1@gmail.com>
2020-07-01 09:35:36 +0900

    django 4강

--- dreamary_project/urls.py
+++ dreamary_project/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from page import views
 from django.conf import settings
-from django.conf.urls.static import staitc
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = "home"),
     path('introduce/', views.introduce, name = "introduce"),
     
-] + static(settings.MEDIA_URL, document_root = settings.NEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
