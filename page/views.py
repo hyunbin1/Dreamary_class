49571e0d517c4c7ea013477766e2246d27e0be38
hyunbin1 <kimgusqls1@gmail.com>
2020-07-03 17:15:06 +0900

    detail.html

--- page/views.py
+++ page/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Designer
 # Create your views here.
 def home(request):
     designers = Designer.objects.all()
     return render(request, 'home.html', {'designers' : designers})
 
-def introduce(requset):
+def introduce(request):
     return render(requset, 'introduce.html')
+
+def detail(request, designer_id):
+    designer = get_object_or_404(Designer, pk = designer_id)
+    return render(request, 'detail.html', {'designer' : designer})
